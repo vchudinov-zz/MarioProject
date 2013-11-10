c807921a0602df72bdbadb084f3806c2dc631e1d
SimonTC <stclementgit@gmail.com>
2013-11-10 21:27:45 +0100

    Added green mushrooms to possible return values when getting enemies

--- src/ch/idsia/benchmark/mario/engine/GeneralizerEnemies.java
+++ src/ch/idsia/benchmark/mario/engine/GeneralizerEnemies.java
@@ -66,6 +66,7 @@ public static byte ZLevelGeneralization(byte el, int ZLevel)
                 case (Sprite.KIND_FIRE_FLOWER):
                     return Sprite.KIND_FIRE_FLOWER;
                 case (Sprite.KIND_MUSHROOM):
+                case (Sprite.KIND_GREEN_MUSHROOM): //Added 10-11-13 by STC
                     return Sprite.KIND_MUSHROOM;
                 case (Sprite.KIND_FIREBALL):
                     return Sprite.KIND_FIREBALL;
@@ -98,6 +99,7 @@ public static byte ZLevelGeneralization(byte el, int ZLevel)
                 case (Sprite.KIND_MARIO):
                 case (Sprite.KIND_FIRE_FLOWER):
                 case (Sprite.KIND_MUSHROOM):
+                case (Sprite.KIND_GREEN_MUSHROOM): //Added 10-11-13 by STC
                     return Sprite.KIND_NONE;
                 case (Sprite.KIND_BULLET_BILL):
                 case (Sprite.KIND_GOOMBA):
