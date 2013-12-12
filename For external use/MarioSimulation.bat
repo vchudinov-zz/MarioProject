bf0fe6cc9340aff33ad53467b5b0919966032b0c
SimonTC <stclementgit@gmail.com>
2013-12-12 17:24:08 +0100

    Deleted more unneeded files

--- For external use/MarioSimulation.bat
+++
@@ -1,1 +0,0 @@
-java -jar MarioSimulator.jar "-vis on -lb on -lca off -lco on -lde on -le off -lf off -lg on -lhs off -ltb off" 0 40 1 1 7 7 "D:\Users\Simon\Documents\MarioFun\NEAT data\Training data\Testing jar\testGenome.txt" AgentScannerNEATSlow
